c644c885a123064be5cdc3bccec10beccf5d6ee3
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-10-19 08:16:39 +0100

    installed next js

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  googleClientID: process.env.googleClientID,
-  googleClientSecret: process.env.googleClientSecret,
-  mongoURI: process.env.mongoURI,  
-  cookieKey: process.env.cookieKey,
-};
