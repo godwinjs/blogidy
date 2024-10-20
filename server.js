92e334a518a3a95cb0f8458ed20a82ebbb0842ea
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-10-20 04:31:02 +0100

    backend added
    express logic updated, ignored .env.
    need to use a better mongo connection code later, for better error handling.

--- server.js
+++ server.js
@@ -1,36 +1,70 @@
 // server.js
 const express = require('express');
 const next = require('next');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+//
+require('dotenv').config();
+//
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+
+require('./models/User');
+require('./models/Blog');
+require('./services/passport');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
 
 // Check if the app is in development mode
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const server = next({ dev });
+const handle = server.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express();
 
-  // Example of a page url request that renders a nextjs page url
-  server.get('/p/:id', (req, res) => {
+server.prepare().then(() => {
+    const app = express();
+
+    // middlewares
+    app.use(bodyParser.json());
+    app.use(
+        cookieSession({
+          maxAge: 30 * 24 * 60 * 60 * 1000,
+          keys: [keys.cookieKey]
+        })
+      );
+    app.use(passport.initialize());
+    app.use(passport.session());
+    
+    // routes
+    require('./routes/authRoutes')(app);
+    require('./routes/blogRoutes')(app);
+    
+    // Example of a page url request that renders a nextjs page url
+    app.get('/p/:id', (req, res) => {
     const actualPage = '/blogs';
     const queryParams = { id: req.params.id }; // Send id as a query parameter
-    app.render(req, res, actualPage+ '/' + queryParams.id);
-  });
+    app.render(req, res, actualPage+ '/' + queryParams.id); // next route:
+    });
 
-   // Example of a custom route using Express
-    server.get('/api/custom', (req, res) => {
+    // express route: Example of a custom route using Express
+    app.get('/api/custom', (req, res) => {
         res.json({ message: 'This is a custom API route using Express!' });
     });
 
-  // Default request handler for all other routes
-  server.all('*', (req, res) => {
-    return handle(req, res);
-  });
+    // Default request handler for all other routes
+    app.all('*', (req, res) => {
+        return handle(req, res);
+    });
 
-  // Start the server on a specific port
-  const PORT = process.env.PORT || 3000;
-  server.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`Server ready on http://localhost:${PORT}`);
-  });
+    // Start the server on a specific port
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, (err) => {
+        if (err) throw err;
+            console.log(`Server ready on http://localhost:${PORT}`);
+        }
+    );
 });
+
+// ref: https://github.com/balmasi/migrate-mongoose
