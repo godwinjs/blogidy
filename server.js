72aa18b1ea14bb6dd1d41f42a16df19702766381
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-10-20 01:38:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  // Example of a custom route using Express
+  // Example of a page url request that renders a nextjs page url
   server.get('/p/:id', (req, res) => {
-    const actualPage = '/post';
+    const actualPage = '/blogs';
     const queryParams = { id: req.params.id }; // Send id as a query parameter
-    app.render(req, res, actualPage, queryParams);
+    app.render(req, res, actualPage+ '/' + queryParams.id);
   });
 
-   // server.js (continued)
+   // Example of a custom route using Express
     server.get('/api/custom', (req, res) => {
         res.json({ message: 'This is a custom API route using Express!' });
     });
