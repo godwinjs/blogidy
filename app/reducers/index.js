ca37abe793f9bb358582cbb29f3e80956a4ed35c
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-10-25 15:21:05 +0100

    Update index.js

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
 import blogsReducer from './blogsReducer';
+import appStateReducer from './appStateReducer'
 
 const rootReducer = combineReducers({
   auth: authReducer,
   form: reduxForm,
-  blogs: blogsReducer
+  blogs: blogsReducer,
+  appState: appStateReducer
 });
 
 export default rootReducer;
