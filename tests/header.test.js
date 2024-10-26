3c947a6c55a565b311d31468098d0eaf31b81739
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-10-26 21:23:25 +0100

    Update header.test.js

--- tests/header.test.js
+++ tests/header.test.js
@@ -14,6 +14,15 @@ test("We can luanch a browser", async () => {
     const page = await browser.newPage();
     page.goto('http://localhost:3000')
 
+    const logoText = await page.locator('a.brand-logo').waitHandle();
+    
+    const text = await logoText?.evaluate((el) => {
+        console.log(el)
+        return el.textContent
+    })
+    
+    expect(logoText).toEqual("Blogidy")
+    console.log(text)
 
-
+    // browser.close()
 })
