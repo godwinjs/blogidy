fabb4abf6c4f593b8327023c2e82c3f23276c93e
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-10-26 19:39:02 +0100

    Update blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+// const {clearHash} = require('../services/cache')
+const cleanCache = require('../middlewares/cleanCache')
 
 const Blog = mongoose.model('Blog');
 
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+    req.user = { id: "6713527cfba9cb302476345d"}
+
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id
@@ -14,31 +18,19 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    // redis
-    const redis = require('redis')
-    const redisUrl = 'redis://127.0.0.1:6379'
-    const client = redis.createClient(redisUrl)
-    client.connect()
+
+    req.user = { id: "6713527cfba9cb302476345d"}
 
     // mongo
-    const blogs = await Blog.find({ _user: req.user.id });
-    // redis: get cached data that exist in the redisDB for this query [req.user.id]
-    const cachedBlogs = await client.get(req.user.id)
+    const blogs = await Blog.find({ _user: req.user.id }).cache({key: req.user.id});
 
-    // if cashed data exists, res to this req with the cached data
-    if(cachedBlogs) {
-      console.log('serving ' + req.user.id +' from cache')
-      return res.send(JSON.parse(cachedBlogs))
-    }
-
-    // if not, respond to the request with the data from mongoDB and update/cache the data for the query in redisDB
-    console.log('serving from mongoDB')
     res.send(blogs);
-    client.set(req.user.id, JSON.stringify(blogs))
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post('/api/blog', requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
+    
+    req.user = { id: "6713527cfba9cb302476345d"}
 
     const blog = new Blog({
       title,
@@ -52,5 +44,8 @@ module.exports = app => {
     } catch (err) {
       res.send(400, err);
     }
+
+    // clearHash(req.user.id);
+
   });
 };
