427166a3a47bdf6bd433088f48a4b831e3979994
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-10-28 19:22:33 +0100

    Update header.test.js

--- tests/header.test.js
+++ tests/header.test.js
@@ -42,7 +42,36 @@ test('clicking login starts the google 0auth flow', async () => {
 }, 15000)
 
 test('When signed in, show logout button.', async () => {
+    const { v4: uuidv4 } = require('uuid');
+    const { v5: uuidv5 } = require('uuid')
+    
+    const namespace = uuidv4().toString();
+    
+    // const { sign } = require('express-session/node_modules/cookie-signature');
+    const { sign } = require('../node_modules/cookie-signature')
+    const Buffer = require('safe-buffer').Buffer;
+    /*
+    {
+    cookie: {
+        path: '/',
+        _expires: 2024-10-28T17:40:48.560Z,
+        originalMaxAge: 86400000,
+        httpOnly: true,
+        secure: false
+    },
+    passport: { user: '6713527cfba9cb302476345d' }
+    }
+    'cGFzc3BvcnQ6IHsgdXNlcjogJzY3MTM1MjdjZmJhOWNiMzAyNDc2MzQ1ZCcgfQ=='
+    '1df6ccba-1599-4b63-889a-42db20ee00e8'
 
+    */
+    const sessionObject = {
+        passport: { user: '6713527cfba9cb302476345d' }
+    }
+    const user = '6713527cfba9cb302476345d';
+    const token = Buffer.from(sessionObject).toString('base64')
+    console.log(uuidv5('cGFzc3BvcnQ6IHsgdXNlcjogJzY3MTM1MjdjZmJhOWNiMzAyNDc2MzQ1ZCcgfQ', namespace ))
+    console.log(sign(uuidv4(), keys.cookieKey))
 
 }, 15000)
 
