175370a3ecb5001162c74b31c68b8388eea72ca7
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-10-29 19:18:36 +0100

    Update sessionFactory.js

--- tests/factories/sessionFactory.js
+++ tests/factories/sessionFactory.js
@@ -2,9 +2,9 @@
 const { v4: uuidv4 } = require('uuid');
 const { v5: uuidv5 } = require('uuid')
 require('dotenv').config();
-const keys = require('../config/keys')
+const keys = require('../../config/keys')
 
-const { sign } = require('../node_modules/cookie-signature')
+const { sign } = require('../../node_modules/cookie-signature')
 const Buffer = require('safe-buffer').Buffer;
 
 module.exports = (id) => {
