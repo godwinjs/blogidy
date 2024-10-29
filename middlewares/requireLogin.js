390bef95ce08a6c1bd459d1d767df61fc1331f92
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-10-29 17:44:48 +0100

    Update requireLogin.js

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,4 +1,10 @@
 module.exports = (req, res, next) => {
+  console.log("X-User-Data", req.headers['x-user-data'])
+  
+  if(req.headers['x-user-data']) {
+    req.user = JSON.parse(req.headers['x-user-data'])
+  }
+
   if (!req.user) {
     return res.status(401).send({ error: 'You must log in!' });
   }
