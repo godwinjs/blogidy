e91655bd1de7935d9b5a148276ff034599f003d6
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-11-01 20:59:38 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,33 @@
+// // 
+// async function querySelector(cssSelector, page) {
+//     return await page.locator(cssSelector);
+// }
+// // 
+// async function querySelectorAll(cssSelector) {
+
+// }
+
+// // returns needed variables
+// async function variables(page) {
+
+//     if(page?.url() === 'https://lights.devfestlagos.com/game'){
+//         return await page.evaluate(() => {
+//             return {
+//                 isPlaying: window.vars.isPlaying,
+//                 gameSequence: window.vars.gameSequence,
+//                 playerSequence: window.vars.playerSequence,
+//                 gameTimer: window.vars.gameTimer,
+//                 isGameInProgress: window.varsisGameInProgress,
+//                 currentLevel: window.vars.currentLevel,
+//                 colorTokens: window.vars.colorTokens,
+//                 level: window.vars.level
+//             }
+//           });
+//     }else{
+//         return null
+//     }
+// }
+
 
 const { initialize } = require('passport');
 const puppeteer = require('puppeteer')
@@ -37,23 +67,28 @@ async function getPage(){
     }
     // await page.waitForNavigation()
 }
-// 
-async function querySelector(cssSelector, page) {
-    return await page.locator(cssSelector);
-}
-// 
-async function querySelectorAll(cssSelector) {
 
-}
 
-async function clickSequence(){
-    let sequences = vars.gameSequence;
-    console.log(vars)
+async function clickSequence(page){
+    let gameSequence = await page.evaluate(() => gameSequence);
+    console.log('clickSeq > gameSequence', gameSequence)
 
-    for ( let i = 0; i < sequences.length; i++){
+    for ( let i = 0; i < gameSequence.length; i++){
 
-        console.log('cliked sequence...', )
-        page.click(`div[data-token="${sequences[i]}"]`, { delay: 200, clickCount: 1})
+        try {
+            const button = await page.$(`div[data-token="${gameSequence[i]}"]`);
+            if (button) {
+                await button.click();
+                await page.evaluate(() => new Promise(resolve => setTimeout(resolve, 300)));
+                // await button.waitForSelector(`active`)
+                // await page.click(`div[data-token="${gameSequence[i]}"]`, { delay: 300, clickCount: 1})
+                console.log('cliked sequence...', gameSequence[i])
+            } else {
+                console.error(`Button not found for element ${gameSequence[i]}`);
+            }
+        } catch (error) {
+            console.log('Error clicking the element:', error.message);
+        }
 
     }
 
@@ -66,32 +101,12 @@ async function clickSequence(){
     //     stopClick = false
     // }
 }
-// returns needed variables
-async function variables(page) {
-
-    if(page?.url() === 'https://lights.devfestlagos.com/game'){
-        return await page.evaluate(() => {
-            return {
-                isPlaying: window.vars.isPlaying,
-                gameSequence: window.vars.gameSequence,
-                playerSequence: window.vars.playerSequence,
-                gameTimer: window.vars.gameTimer,
-                isGameInProgress: window.varsisGameInProgress,
-                currentLevel: window.vars.currentLevel,
-                colorTokens: window.vars.colorTokens,
-                level: window.vars.level
-            }
-          });
-    }else{
-        return null
-    }
-}
 
 async function playGame(page, clrInt){
-    const isPlayed = await page.evaluate(() => isPlaying)
-    console.log("isPlaying", isPlayed)
+    // const isPlayed = await page.evaluate(() => isPlaying)
+    // console.log("isPlaying", isPlayed)
 
-    console.log("Game strarting.....", vars)
+    console.log("Game strarting.....")
     // await page.waitForNavigation()
     
     // page.reload()
@@ -103,61 +118,31 @@ async function playGame(page, clrInt){
     }
 
     // const variable = await variables(page);
-    if(Object.keys(vars).length <= 0){
-        console.log('Game not currently in play mode, variables not loaded')
-        return;
-    }
+    // if(Object.keys(vars).length <= 0){
+    //     console.log('Game not currently in play mode, variables not loaded')
+    //     return;
+    // }
 
-    const { gameSequence, level, isPlaying} = vars;
+    // const { gameSequence, level, isPlaying} = vars;
 
-    function answer() {
-        let ran = false;
-        console.log(level, desiredLevel, gameSequence)
-        if( level <= desiredLevel && ran === false){
-            console.log('Yippie')
-            if(isPlaying ){
-                clickSequence()
-            }
-        }else{
-            ran = true;
-        }
-    }
-    await page.exposeFunction('answer', answer);
+    // function answer() {
+    //     let ran = false;
+    //     console.log(level, desiredLevel, gameSequence)
+    //     if( level <= desiredLevel && ran === false){
+    //         console.log('Yippie')
+    //         if(isPlaying ){
+    //             clickSequence()
+    //         }
+    //     }else{
+    //         ran = true;
+    //     }
+    // }
+    // await page.exposeFunction('answer', answer);
 
 }
 
 (async function initialize(){
     const page = await getPage();
-    
-    const mainFrame = page.mainFrame();
-    let mainFrameVars;
-    
-    if(page.url() === 'https://lights.devfestlagos.com/game' || page.url() === 'https://lights.devfestlagos.com/game#'){
-        mainFrameVars = await mainFrame.evaluate(() => {
-            return {
-                isPlaying,
-                gameSequence,
-                playerSequence,
-                gameTimer,
-                isGameInProgress,
-                currentLevel,
-                colorTokens,
-                level
-            }
-        });
-    }
-
-    const clearMainFrameVarsInt = setInterval( () => {
-        
-        vars = mainFrameVars
-    
-    }, 1000)
-    
-    await playGame(page)
-
-    await page.exposeFunction('vars', function varsFunc() {
-        return vars
-    });
 
     // Listen for navigation events in the main frame and all child frames
     page?.on('framenavigated', async (frame) => {
@@ -165,16 +150,20 @@ async function playGame(page, clrInt){
             console.log('User navigated to:', frame.url());
             if( frame.url() === 'https://lights.devfestlagos.com/game' ){
                 // await mainFrameVars(mainFrame)
-                await playGame(page, clearMainFrameVarsInt)
+                await addEvents(page)
+                await playGame(page)
             }
             if( frame.url() === 'https://lights.devfestlagos.com/game#'){
-                console.log(vars)
-                await clickSequence(page, vars, clearMainFrameVarsInt)
+                await clickSequence(page)
             }
 
         }
     });
 
+    async function addEvents() {
+        
+    }
+    
     page.waitForSelector('div.controls', { timeout: 0})
 
     const controls = await page.$$eval('div.controls', (El) => El.map(E => {
@@ -187,20 +176,43 @@ async function playGame(page, clrInt){
             leaderboardBtn.textContent = "Answer";
             leaderboardBtn.setAttribute('href', '#');
 
-            console.log('answer button and variables prepared..', window.vars)
+            console.log('listeneer > answer button and variables prepared..', window.vars)
         })
-        leaderboardBtn.addEventListener('click', window.answer)
+        // leaderboardBtn.addEventListener('click', window.answer)
 
         return [startBtn.textContent, leaderboardBtn.textContent]
     }))
+    console.log('controls', controls)
 
-    console.log(controls)
-    
-
-    page?.on('domcontentloaded', async () => {
-        if( page.url() === 'https://lights.devfestlagos.com/game' ){
-            // await playGame(page)
-        }
-    })
     // console.log(page)
 })()
+
+// const mainFrame = page.mainFrame();
+//     let mainFrameVars;
+    
+//     if(page.url() === 'https://lights.devfestlagos.com/game' || page.url() === 'https://lights.devfestlagos.com/game#'){
+//         mainFrameVars = await mainFrame.evaluate(() => {
+//             return {
+//                 isPlaying,
+//                 gameSequence,
+//                 playerSequence,
+//                 gameTimer,
+//                 isGameInProgress,
+//                 currentLevel,
+//                 colorTokens,
+//                 level
+//             }
+//         });
+//     }
+
+//     const clearMainFrameVarsInt = setInterval( () => {
+        
+//         vars = mainFrameVars
+    
+//     }, 1000)
+    
+//     await playGame(page)
+
+//     await page.exposeFunction('vars', function varsFunc() {
+//         return vars
+//     });
