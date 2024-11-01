92d1fb32c768a2692283493b41f298ba865d906d
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-11-01 16:51:07 +0100

    Update gameServer.js

--- gameServer.js
+++ gameServer.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer');
 // console.log(process)
 
 if (cluster.isMaster) {
-    let browser, page; // This will hold the Puppeteer browser instance
+    let browser, page, wsEndpoint; // This will hold the Puppeteer browser instance
     // Launch the browser when the master starts
     async function launchBrowser(url) {
         browser = await puppeteer.launch({
@@ -15,32 +15,35 @@ if (cluster.isMaster) {
         })
         page = await browser.newPage();
     
-        const wsEndpoint = browser.wsEndpoint();
-        console.log('WebSocket Debugger URL:', wsEndpoint);
+        wsEndpoint = browser.wsEndpoint();
     
         await page.goto(url, { waitUntil: 'domcontentloaded' })
         // await page.waitForNavigation()
 
         // Fork child processes
-        const child = fork('./game.js', { data: wsEndpoint});
-        child.send({ message: 'page data', data: wsEndpoint})
+        // const child = fork('./game.js', { data: wsEndpoint});
+        // child.send({ message: 'page data', data: wsEndpoint})
         
         nodemon.on('restart', () => {
             console.log('Restarting child process...');
             child.kill(); // Kill the current child process
             const newChild = fork('./game.js'); // Fork a new child process
             newChild.send({ message: 'page data', data: wsEndpoint})
-            // You can set up additional IPC here if needed'
+            //set up additional IPC here if needed'
             // nodemon({ script: './game.js' });
 
         });
+
+        return new Promise((resolve) => {
+            // Restart the child process on file changes
+            const nodemon = require('nodemon');
+            nodemon({ script: './game.js', env: { WSE: wsEndpoint } });
+            resolve()
+        })
+
     }
 
-    launchBrowser('https://lights.devfestlagos.com/login')
-
-    // Restart the child process on file changes
-    const nodemon = require('nodemon');
-    nodemon({ script: './game.js' });
+    launchBrowser('https://lights.devfestlagos.com')
 
     process.on('exit', async () => {
         if (browser) {
@@ -49,36 +52,6 @@ if (cluster.isMaster) {
     });
 
 } else {
-    // This code will run in the child process
-    require('./game.js'); // Load the child logic here
-}
-
-
-
-
-// if(cluster.isMaster){
-//     // master
-
-//     process.on("SIGHUP", function () {
-//         for (const worker of Object.values(cluster.workers)) {
-//           worker.process.kill("SIGTERM");
-//         }
-//     });
-
-//     cluster.fork()
-
-// }else{
-//     // children process
-//     console.log(browser)
-
-// }
-
-
-function runOnceOnRestart() {
-    console.log("Game restarted...");
-}
-async function getAllcircles(){
-    const circles = await page.locator('.light-grid');
-
-    return circles;
+    // code will run in the child process
+    // require('./game.js');
 }
